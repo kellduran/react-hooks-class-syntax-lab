8f92d1e6fbb728952e3b4e145b13042a73aa9037
Kelly Duran <kaduran16@gmail.com>
2022-08-04 17:27:09 -0400

    commit tree d803dc8b469e0316c5cbccb59b02be5cc63f7f25

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-    </nav>
-  );
+// function NavBar() {
+//   return (
+//     <nav>
+//       <a href="#home">Home</a>
+//       <a href="#about">About</a>
+//     </nav>
+//   );
+// }
+
+// export default NavBar;
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    )
+  }
 }
-
-export default NavBar;
